daaccdf17e122420f933395725154831049294b3
Maheshbilla03 <maheshbilla03@gmail.com>
2026-02-03 16:04:07 +0530

    javascript tasks updated

---
+++ task2.js
@@ -0,0 +1,30 @@
+// 1. welome progam //
+
+let userName = prompt("Enter your name:");
+let welcomeMessage = "Hi " + userName + ", Welcome to JavaScript Training.";
+
+//alert(welcomeMessage);
+console.log(welcomeMessage);
+
+//2.console mothesds //
+console.log("500");
+console.warn("500");
+console. error("500");
+
+
+let str = "JavaScript";
+let num = 10;
+let bool = true;
+let undef;
+let nul = null;
+
+console.log(typeof(str))
+console.log(typeof(num))
+console.log(typeof(bool))
+console.log(typeof(undef))
+console.log(typeof(nul))
+
+
+
+
+
